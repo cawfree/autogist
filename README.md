03f18c9e71a4e51316ec7f02aad730519dc1eec5
Alex Thomas (@cawfree) <hello@cawfree.com>
2020-01-13 10:23:51 +0000

    Published to GitHub! 🚀

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # autogist
 ⚡📄🥯Sync a GitHub Gist.
+
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```bash
+npm i -g autogist
+```
+
+Next, if you haven't already, you need to [create a new GitHub OAuth2 Token](https://developer.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/) with the `gist` permission. You can add a token to your profile [here](https://github.com/settings/tokens/new).
+
+Finally, you need to add this token to your environment variables.
+
+#### OSX
+
+```bash
+echo "AUTOGIST_TOKEN <your-token-here>" >> ~/.bash_profile
+source ~/.bash_profile
+```
+
+#### Linux
+```bash
+echo "AUTOGIST_TOKEN <your-token-here>" >> ~/.bashrc
+source ~/.bashrc
+```
+
+## 🗞️ Publishing via the Commnand Line
+
+### Create a Gist
+
+To create a new gist, call `autogist` with the description (`-d`) and content (`-c`) of your gist.
+
+```bash
+npx autogist -d "gist description" -c "gist content"
+```
+
+### Update a Gist
+
+To update a gist, just specify the id (`-g`) of your gist.
+
+```bash
+npx autogist -d "gist description" -c "updated content" -g <my-gist-id>
+```
+
+## ✌️ License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ src/index.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+import '@babel/polyfill';
+
+import yargs from 'yargs';
+import axios from 'axios';
+import chalk from 'chalk';
+
+const url = 'https://api.github.com/gists';
+const charLength = 56;
+const defaultDescription = '📈 GitHub Status';
+
+const { AUTOGIST_TOKEN } = process.env;
+
+const baseRequest = (sfx = '') => ({
+  url: `${url}${sfx}`,
+  headers: {
+    'Authorization': `token ${AUTOGIST_TOKEN}`,
+    'Content-Type': 'application/json',
+  },
+});
+
+const formatGist = (visible=true, name='autogist', description=defaultDescription, content) => ({
+  files: {
+    [name]: {
+      content,
+    },
+  },
+  description,
+  public: visible,
+});
+
+const createGist = (description, content) => axios(
+  {
+    ...baseRequest(),
+    method: 'post', 
+    data: formatGist(true, undefined, description, content),
+  },
+)
+  .then(({ data: { id } }) => id);
+
+const updateGist = (gistId, description, content) => axios(
+  {
+    ...baseRequest(`/${gistId}`),
+    method: 'patch',
+    data: formatGist(true, undefined, description, content),
+  },
+);
+
+const { argv } = yargs
+  .option(
+    'gist',
+    {
+      alias: 'g',
+      type: 'text',
+      description: 'Identifier of the gist. (Default: create new)',
+    },
+  )
+  .option(
+    'description',
+    {
+      alias: 'd',
+      type: 'text',
+      description: `Descripton of the gist. (Default: "${defaultDescription}")`,
+    },
+  )
+  .option(
+    'content',
+    {
+      alias: 'c',
+      type: 'text',
+      description: 'Content of the gist.',
+    },
+  );
+
+let { gist, description, content } = argv;
+
+(
+  async () => {
+    if (typeof AUTOGIST_TOKEN !== 'string' || AUTOGIST_TOKEN.length <= 0) {
+      console.error(chalk.yellow(`It looks like you haven\'t set the AUTOGIST_TOKEN environment variable. Head over to https://github.com/cawfree/autogist for more info.`));
+    } else if (gist === undefined) {
+      gist = await createGist(
+        description,
+        content,
+      );
+      console.log(chalk.green(`Created a new gist! (${gist})`));
+    } else if (typeof gist === 'string' && gist.length > 0) { // TODO: should test the gist id
+      await updateGist(
+        gist,
+        description,
+        content,
+      );
+      console.log(chalk.green('Updated!'));
+    } else {
+      console.error(chalk.red(`Expected valid gist, encountered ${gist}.`));
+    }
+  }
+)();
